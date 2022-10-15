1f955ab3797ac427281d9f7e0a037eb03bb5b1a1
Fernando <a52hepof@uco.es>
2022-10-15 12:47:59 +0200

    modifying build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requeriments.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput
-python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('fernando', 'admin@example.com', '1')"
+python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('324', 'admin@example.com', '1')"
