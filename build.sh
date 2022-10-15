c61fb8ea4c4497e6e4adb309e94d3f291fd8d635
Fernando <a52hepof@uco.es>
2022-10-15 13:01:18 +0200

    modifying build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ pip install -r requeriments.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput
-python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('324', 'admin@example.com', '1')"
+#create superusers. Primer despliegue
+#python manage.py createsuperuser --noinput #take enviroment variables
+#python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('324', 'admin@example.com', '1')"
