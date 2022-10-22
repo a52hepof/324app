1657dd11e0d55baeb972b37296a3807ed7f0fc29
Fernando <a52hepof@uco.es>
2022-10-22 17:06:11 +0200

    reset

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -o errexit
 pip install -r requeriments.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+#python manage.py makemigrations
+python manage.py migrate --fake-initial
 #create superusers. Primer despliegue 
 #python manage.py createsuperuser --noinput #take enviroment variables
 #python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('324', 'admin@example.com', '1')"
